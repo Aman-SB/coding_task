cfca1b9a595e2c88fff17acf0b4ed7e30e19ead0
Aman Bisht <bishtaman60@gmail.com>
2024-10-10 12:21:31 +0530

    complete task1 (#2) Route setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Form from './Components/Form';
+import UserData from './Components/UserData';
 
 export default function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/form" element={<Form/>}></Route>
+        <Route path="/userdata" element={<UserData/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
